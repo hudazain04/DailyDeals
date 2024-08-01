6af873d8d79ed668c4bda84e422f68a82cb09150
hudazain04 <huda1812zain@gmail.com>
2024-08-01 14:01:12 +0300

    add admin user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Types\UserType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,6 +18,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(10)->create();
+        User::factory()->count(1)->create(['type'=>UserType::Admin]);
     }
 }
