c0a04882749aee332ae77f022c19f899e893ff57
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-08-15 11:28:09 +0300

    add some styles to image of qr code genrated

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -40,8 +40,18 @@ class BranchController extends Controller
         for ($rating = 1; $rating <= 5; $rating++) {
             $url = route('rateBranch', ['branch_id' => $branch->id, 'rate' => $rating]);
 
-            $qrCode = QrCode::format('png')->size(300)->generate($url);
 
+//            add some styles to the qr code generated
+            $from = [255, 0, 0];
+            $to = [0, 0, 255];
+            $qrCode =QrCode::size(200)
+                ->style('dot')
+                ->eye('circle')
+                ->gradient($from[0], $from[1], $from[2], $to[0], $to[1], $to[2], 'diagonal')
+                ->margin(1)
+                ->generate($url);
+
+//            make it unique by uuid or timestamp
             $fileName = 'branch_' . $branch->id . '_rating_' . $rating . '.png';
 
 
