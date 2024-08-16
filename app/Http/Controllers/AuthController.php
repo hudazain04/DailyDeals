7df7967308773bb812e3067ef0ce02f6c1c68ea8
masaaj <masaajlany@gmail.com>
2024-08-16 22:32:48 +0300

    remove verification from register and resend code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,7 +62,7 @@ class AuthController extends Controller
             }
             $code=$this->SendVerificationCode($user,VerificationCodeType::register_code);
             DB::commit();
-            return $this->success(['user' => UserResource::make($user),'verification_code'=>$code],__('messages.auth_controller.register'));
+            return $this->success(['user' => UserResource::make($user)],__('messages.auth_controller.register'));
         }catch (\Throwable $th){
             DB::rollBack();
             return $this->error($th->getMessage(),500);
@@ -134,7 +134,7 @@ class AuthController extends Controller
                 return $this->error(__('messages.email_not_found'),404);
             }
             $code=$this->sendVerificationCode($user,VerificationCodeType::register_code);
-            return $this->success(['user' => UserResource::make($user),'verification_code'=>$code] ,__('messages.auth_controller.resend_code'));
+            return $this->success(['user' => UserResource::make($user)] ,__('messages.auth_controller.resend_code'));
         }catch (\Throwable $th){
             return $this->error($th->getMessage(),500);
         }
