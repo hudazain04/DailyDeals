9f61c11cba14cb2c43cffbd61351ae33215e99af
hudazain04 <huda1812zain@gmail.com>
2024-08-17 14:24:21 +0300

    offers

--- app/Http/Resources/OfferResource.php
+++ app/Http/Resources/OfferResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Discount_Offer;
+use App\Models\Extra_Offer;
+use App\Models\Percentage_Offer;
 use App\Types\OfferType;
 use App\Types\UserType;
 use Illuminate\Http\Request;
@@ -16,6 +19,8 @@ class OfferResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+//        dd($this->gift_offer());
+
         $baseData=[
             'id'=>$this->id,
 //            'name'=>$this->name,
@@ -24,17 +29,22 @@ class OfferResource extends JsonResource
         ];
         switch ($this->type)
         {
-
             case OfferType::Percentage:
-                $baseData = array_merge($baseData, ['percentage' => $this->percentage_offer() ? $this->percentage_offer()->percentage : null]);
+//                $percentageOffer = $this->percentage_offer();
+                $percentageOffer=Percentage_Offer::where('offer_id',$this->id)->first();
+                $baseData = array_merge($baseData, ['percentage' => $percentageOffer ? $percentageOffer->percentage : null]);
                 break;
             case OfferType::Discount:
-                $baseData = array_merge($baseData, ['discount' => $this->discount_offer() ? $this->discount_offer()->discount : null]);
+//                $discountOffer = $this->discount_offer();
+                $discountOffer=Discount_Offer::where('offer_id',$this->id)->first();
+                $baseData = array_merge($baseData, ['discount' => $discountOffer ? $discountOffer->discount : null]);
                 break;
             case OfferType::Extra:
+//                $extraOffer = $this->extra_offer();
+                $extraOffer=Extra_Offer::where('offer_id',$this->id)->first();
                 $baseData = array_merge($baseData, [
-                    'product_count' => $this->extra_offer() ? $this->extra_offer()->product_count : null,
-                    'extra_count' => $this->extra_offer() ? $this->extra_offer()->extra_count : null,
+                    'product_count' => $extraOffer ? $extraOffer->product_count : null,
+                    'extra_count' => $extraOffer ? $extraOffer->extra_count : null,
                 ]);
                 break;
         }
--- database/seeders/OfferSeeder.php
+++ database/seeders/OfferSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Discount_Offer;
+use App\Models\Extra_Offer;
+use App\Models\Gift_Offer;
 use App\Models\Offer;
+use App\Models\Percentage_Offer;
+use App\Types\OfferType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +19,27 @@ class OfferSeeder extends Seeder
     public function run(): void
     {
         Offer::factory()->count(10)->create();
+        Offer::factory()
+            ->count(10)
+            ->create()
+            ->each(function ($offer) {
+                switch ($offer->type) {
+                    case OfferType::Percentage:
+                        Percentage_Offer::factory()->create(['offer_id' => $offer->id]);
+                        break;
 
+                    case OfferType::Discount:
+                        Discount_Offer::factory()->create(['offer_id' => $offer->id]);
+                        break;
+
+                    case OfferType::Extra:
+                        Extra_Offer::factory()->create(['offer_id' => $offer->id]);
+                        break;
+
+                    case OfferType::Gift:
+                        Gift_Offer::factory()->create(['offer_id' => $offer->id]);
+                        break;
+                }
+            });
     }
 }
